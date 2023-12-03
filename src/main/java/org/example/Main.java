811916aff5bfc7e767da157e85351379a252fa90
4767300 <dominikfilatov@gmail.com>
2023-12-03 15:55:30 +0100

    03_12_23
    Signed-off-by: Oruon <dominikfilatov@gmail.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,42 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static Scanner scan = new Scanner(System.in);
-    public static int[] randomNumber = new int[6];
-    public static int[] number = new int [6];
+    public static ArrayList<Integer> randomNumber = new ArrayList<>();
+    public static ArrayList<Integer> number = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Prosze wpisac wybrane prszez was licbe od 1 - 49: ");
-        for (int i = 0; i < number.length; i++) {
-            number[i] = scan.nextInt();
-        }
-        System.out.println(Arrays.toString(number));
-    TotolotoNumber();
-        System.out.println(Match.matchingNumbers(number, randomNumber));
+            for (int i = 0; i <= number.size(); i++) {
+                if (number.size() < 6) {
+                    number.add(scan.nextInt());
+                }
+            }
+            System.out.println(number);
+             TotolotoNumber();
+            System.out.println(Match.matchingNumbers(number, randomNumber));
 
     }
 
 
     public static void TotolotoNumber(){
-        for (int i = 0; i < randomNumber.length; i++) {
-            randomNumber[i] = (int) Math.round(Math.random() * 50);
+        for (int i = 0; i < randomNumber.size(); i++) {
+            if (randomNumber.size() < 6) {
+                randomNumber.add((int) Math.round(Math.random() * 50));
+            }
         }
-        System.out.println(Arrays.toString(randomNumber));
+        System.out.println(randomNumber);
     }
 }
     class Match {
         public static int matches;
-
-        public static int matchingNumbers(int[] a, int[] b) {
-            if (a.length == b.length) {
-                for (int i = 0; i < a.length; i++) {
-                    if (a[i] == b[i])
+        public static int matchingNumbers(ArrayList<Integer> a, ArrayList<Integer> b) {
+            if (a.size() == b.size()) {
+                for (int i = 0; i <= a.size(); i++) {
+                    if (a.get(i).equals(b.get(i)))
                         matches += 1;
                 }
             }
